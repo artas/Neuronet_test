140a445db6724e5cdf2d3b6b9049f7ff0fc40ab3
k.borovkov <k.borovkov@neovox.ru>
2021-07-18 22:46:35 +0300

    initial

---
+++ Server.py
@@ -0,0 +1,60 @@
+import asyncio
+import wave
+
+
+framerate = 48000
+
+BUFFER = {}
+
+
+async def save(data, addr):
+    wf = wave.open(f'{addr}.wav', 'wb')
+    wf.setnchannels(1)
+    wf.setsampwidth(2)
+    wf.setframerate(framerate)
+    wf.writeframes(b''.join(data))
+
+
+HOST, PORT = 'localhost', 9999
+
+
+class Protocol(asyncio.DatagramProtocol):
+    def __init__(self):
+        super().__init__()
+        self.loop = asyncio.get_running_loop()
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+    async def respond(self, data, addr):
+        if data == b'Accept':
+            print('OK')
+            BUFFER.update({addr: []})
+        elif data == b'Close':
+
+            self.loop.create_task(save(BUFFER.get(addr), addr))
+            BUFFER.pop(addr)
+        else:
+            BUFFER.get(addr).append(data)
+
+    def datagram_received(self, data, addr):
+
+        self.loop.create_task(self.respond(data, addr))
+
+
+async def main():
+    print("Starting UDP server")
+
+    loop = asyncio.get_running_loop()
+
+    transport, protocol = await loop.create_datagram_endpoint(
+        lambda: Protocol(),
+        local_addr=('127.0.0.1', 9999))
+
+    try:
+        await asyncio.sleep(3600)  # Serve for 1 hour.
+    finally:
+        transport.close()
+
+
+asyncio.run(main())
---
+++ client.py
@@ -0,0 +1,37 @@
+import time
+import wave
+import sys
+from socket import socket, AF_INET, SOCK_DGRAM
+
+host = '127.0.0.1'
+port = 9999
+connection_params = (host, port)
+
+
+def read_wave_file(filename):
+    """
+    :param filename: Название файла, который надо передать
+    :return: биты
+    """
+    udp_socket = socket(AF_INET, SOCK_DGRAM)
+    udp_socket.sendto(b'Accept', connection_params)
+    start = time.time()
+    try:
+        w = wave.open(filename, 'rb')
+        data = w.readframes(2048)
+        while data:
+
+            if udp_socket.sendto(data, connection_params):
+                data = w.readframes(2048)
+                time.sleep(1/1000)
+        w.close()
+        udp_socket.sendto(b'Close', connection_params)
+    except FileNotFoundError:
+        print(f"Ошибка! Фаил: {filename} не найден")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        read_wave_file(sys.argv[1])
+    else:
+        print('Укажите навзание файла')
